c6bd85f749f25534e0521d607f521196bcded40c
Veresia Hiralall <hiralallveresia@yahoo.com>
2020-12-29 17:22:46 -0500

    commit tree 27d72dc6923bb4aacb938b8833258199b41fa09a

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", "5", "M", "brown", "goofy", "1");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", "10", "M", "grey", "grumpy", "0");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", "6", "F", "brown", "goofy", "1");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", "12", "M", "brown", "grumpy", "0");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", "13", "F", "white", "goofy", "1");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", "2", "M", "brown", "grumpy", "0");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", "6", "F", "white", "goofy", "1");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, "10", "M", "brown", "grumpy", "0");
